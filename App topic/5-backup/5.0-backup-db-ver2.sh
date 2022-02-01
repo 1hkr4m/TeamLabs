6147261f65716c8eb012d481e2afe055d4dbf7fa
Ihor Kram <iproute127@gmail.com>
2022-02-01 16:24:15 +0200

    add vagrantfile

--- App topic/5-backup/5.0-backup-db-ver2.sh
+++ App topic/5-backup/5.0-backup-db-ver2.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
 LOG_FILE="/var/log/mysql_backups_log.txt"
-BACKUP_DIR="~/Backup"
+BACKUP_DIR="/home/vagrant/db_backup"
 
 if [[ "${UID}" -ne 0 ]]
 then
     echo "Plese run as sudo user!"
     exit 1
 else
-    echo "$(date) - You run mysql backup script." >> $LOG_FILE
-    menu_sc
+    echo "$(date) - You run mysql backup script."
 fi
 
 selectdb_backup() {
     
-    if [[ ! -d /var/db_backup ]]
+    if [[ ! -d "${BAKUP_DIR}" ]]
     then
-        mkdir /var/db_backup
+        mkdir $BACKUP_DIR
+        echo "Dir db_backup created!"
     fi
 
-    echo "Enter names of dababases, that you want to backup! Plese, use only space key between names that db1_name db2_name... !"
+    echo "Enter names of dababases, that you want to backup!"
     read SELECTED_DB
 
-    mysqldump -u root -p --databases $SELECTED_DB > $BACKUP_DIR/selected_dump_$(date).sql
+    mysqldump -u root -p --databases $SELECTED_DB > $BACKUP_DIR/mydb.sql
     
     if [[ "${?}" -eq 0 ]]
-        echo "$(date) - Backups operations finish succesfully!" >> $LOG_FILE
+    then
+        echo "$(date) - Backups operations finish succesfully!"
     else
-        echo "$(date) - ERROR! Cant backup you database(s)!" >> $LOG_FILE
+        echo "$(date) - ERROR! Cant backup you database(s)!"
         exit 1
     fi
 }
+
+selectdb_backup
