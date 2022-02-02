4bf6deba33221c1afd42af2c74191ed86f38c257
Ihor Kram <iproute127@gmail.com>
2022-02-02 21:12:13 +0200

    backup script update

--- App topic/5-backup/5.0-backup.sh
+++ App topic/5-backup/5.0-backup.sh
@@ -1,59 +1,51 @@
 #!/bin/bash
 
 LOG_FILE="/var/log/mysql_backups_log.txt"
-BACKUP_DIR="/tmp"
+BACKUP_DIR="/home/vagrant/db_backup"
 
 if [[ "${UID}" -ne 0 ]]
 then
-    echo "Plese run as sudo user!"
+    tput setaf 3; echo "Plese run as sudo user!"; tput sgr0
     exit 1
 else
     echo "$(date) - You run mysql backup script." >> $LOG_FILE
-    menu_sc
 fi
 
-menu_sc() {
+help_f() {
+tput setaf 3
+cat <<eof
+You must enter at least one database name!
 
-    echo "          List"
-    echo "          ------"
-    echo "What you want to backup?:"
-    echo "1. Backup all databases!"
-    echo "2. Backup only selected databases!"
-    echo "3. Exit."
-    echo
-
-    read OPTION
-
-    case ${OPTION} in
-        1) alldb_backup ;;
-        2) selectdb_backup ;;
-        3) exit 0 ;;
-    esac
-}
-
-alldb_backup() {
-
-    mysqldump -u root -p --all-databases > $BACKUP_DIR/alldb_dump_$(date).sql
-
-    if [[ "${?}" -eq 0 ]]
-        echo "$(date) - Backups operations finish succesfully!" >> $LOG_FILE
-    else
-        echo "$(date) - ERROR! Cant backup you databases!" >> $LOG_FILE
-        exit 1
-    fi
+Example: root@localhost:~# 5.0-backup.sh db_name1 db_name2...
+eof
+tput sgr0
+exit 1
 }
 
 selectdb_backup() {
     
-    echo "Enter names of dababases, that you want to backup! Plese, use only space key between names that db1_name db2_name... !"
-    read SELECTED_DB
+    if [[ ! -d "${BAKUP_DIR}" ]]
+    then
+        mkdir $BACKUP_DIR
+        echo "Dir db_backup created!"
+    fi
 
-    mysqldump -u root -p --databases $SELECTED_DB > $BACKUP_DIR/selected_dump_$(date).sql
+    mysqldump -u root -p --databases $@ > $BACKUP_DIR/mydb.sql
     
     if [[ "${?}" -eq 0 ]]
+    then
+        tput setaf 2; echo "$(date) - Backups operations finish succesfully!"; tput sgr0
         echo "$(date) - Backups operations finish succesfully!" >> $LOG_FILE
     else
-        echo "$(date) - ERROR! Cant backup you database(s)!" >> $LOG_FILE
+        tput setaf 1; echo "$(date) - ERROR! Cant backup you database(s)! Check databases names!"; tput sgr0
+        echo "$(date) - Backups operations filed!" >> $LOG_FILE
         exit 1
     fi
 }
+
+if [[ "${#}" -lt 1 ]]
+then
+    help_f
+else
+    selectdb_backup $@
+fi
