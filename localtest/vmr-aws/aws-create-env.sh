3c7ea6760973a66d7eea759d6a436d2f8962f114
Ihor Kram <iproute127@gmail.com>
2022-03-29 15:47:11 +0300

    update aws

--- localtest/vmr-aws/aws-create-env.sh
+++ localtest/vmr-aws/aws-create-env.sh
@@ -27,6 +27,12 @@ aws ec2 authorize-security-group-ingress \
     --port 443 \
     --cidr 0.0.0.0/0 >> /dev/null
 
+aws ec2 authorize-security-group-ingress \
+    --group-id $SG \
+    --protocol tcp \
+    --port 8080 \
+    --cidr 0.0.0.0/0 >> /dev/null
+
 for i in vmr1-web1 vmr1-app1 vmr1-db1 vmr2-web1 vmr2-app1 vmr2-db1
 do
     aws ec2 run-instances --image-id ami-04505e74c0741db8d --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids $SG \
