470c776a968e8e54d1d052c92ddd179eca4c3703
Ihor Kram <iproute127@gmail.com>
2022-03-31 16:12:21 +0300

    localtest update

--- localtest/vmr-aws/aws-create-env.sh
+++ localtest/vmr-aws/aws-create-env.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+aws-create-keypair() {
 aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem
 chmod 400 MyKeyPair.pem
+}
 
+aws-create-sc() {
 aws ec2 create-security-group --group-name MySecurityGroup --description "My security group" >> aws-sc.txt
 
 SG=$(cat aws-sc.txt|grep sg*|cut -c17-36)
@@ -32,9 +35,16 @@ aws ec2 authorize-security-group-ingress \
     --protocol tcp \
     --port 8080 \
     --cidr 0.0.0.0/0 >> /dev/null
+}
 
+aws-create-instances() {
 for i in vmr1-web1 vmr1-app1 vmr1-db1 vmr2-web1 vmr2-app1 vmr2-db1
 do
     aws ec2 run-instances --image-id ami-04505e74c0741db8d --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids $SG \
             --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$i'}]' >> /dev/null
 done
+}
+
+aws-create-keypair
+aws-create-sc
+aws-create-instances
